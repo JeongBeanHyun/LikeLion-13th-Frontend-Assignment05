18845bfedcea6a044b152917167576b860bfe363
JeongBeanHyun <kelly92108@gmail.com>
2025-06-01 19:55:02 +0900

    feat: 프로젝트 추가

---
+++ hjb/form/src/App.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import TodoList from "./components/TodoList";
+import TodoDetail from "./components/TodoDetail";
+
+export default function App(){
+  return(
+    <Router>
+      <h1>Todo List</h1>
+      <Routes>
+        <Route path="/" element={<TodoList/>}/>
+        <Route path="/todo/:id" element={<TodoDetail />}/>
+      </Routes>
+    </Router>
+  );
+}
